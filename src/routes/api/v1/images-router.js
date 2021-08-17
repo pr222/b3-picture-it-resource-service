11d4bd439691be2ecd14559b8698fb03e5c21478
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-08-17 21:54:47 +0200

    Configure server and add routes

--- src/routes/api/v1/images-router.js
+++ src/routes/api/v1/images-router.js
@@ -1,6 +1,68 @@
-// GET   /
-// POST  /
-// GET   /:id
-// PUT   /:id
-// PATCH /:id
-// DEL.. /:id
+/**
+ * Images routes.
+ *
+ * @author Pauliina Raitaniemi <pr222ja@student.lnu.se>
+ * @version 1.0.0
+ */
+import express from 'express'
+import { ImagesController } from '../../../controllers/api/images-controller.js'
+
+export const router = express.Router()
+
+const controller = new ImagesController()
+
+/**
+ * Request authentication.
+ *
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {Function} next - Express next-middleware function.
+ */
+const verifyJWT = (req, res, next) => {
+  // TODO: Check bearer token
+  // TODO: Try verifying token
+}
+
+// TODO: Load image for id param
+
+// GET all images
+router.get('/',
+  verifyJWT(),
+  (req, res, next) =>
+    controller.findAll(req, res, next)
+)
+
+// POST a new image
+router.post('/',
+  verifyJWT(),
+  (req, res, next) =>
+    controller.create(req, res, next)
+)
+
+// GET a specific image
+router.get('/:id',
+  verifyJWT(),
+  (req, res, next) =>
+    controller.find(req, res, next)
+)
+
+// PUT update an image
+router.put('/',
+  verifyJWT(),
+  (req, res, next) =>
+    controller.replace(req, res, next)
+)
+
+// PATCH partially update an image
+router.patch('/',
+  verifyJWT(),
+  (req, res, next) =>
+    controller.modify(req, res, next)
+)
+
+// DELETE a specific image
+router.delete('/',
+  verifyJWT(),
+  (req, res, next) =>
+    controller.delete(req, res, next)
+)
--- src/routes/api/v1/router.js
+++ src/routes/api/v1/router.js
@@ -1,1 +1,15 @@
-// router.use('/images', images-router)
+/**
+ * Routes for API v1.
+ *
+ * @author Pauliina Raitaniemi <pr222ja@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import { router as imagesRouter } from './images-router.js'
+
+export const router = express.Router()
+
+router.get('/', (req, res) => res.json({ message: 'You got Resource API v1!' }))
+
+router.use('/images', imagesRouter)
